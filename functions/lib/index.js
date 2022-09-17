b341a0bed45c1de61d87f6efdf31c9557435654e
Rebero <rmedard5@gmail.com>
2022-09-17 02:26:17 +0200

    first commit

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const stripe_1 = require("stripe");
+const admin = require("firebase-admin");
+const tasks_1 = require("@google-cloud/tasks");
+const protos_1 = require("@google-cloud/tasks/build/protos/protos");
+var Task = protos_1.google.cloud.tasks.v2.Task;
+var HttpMethod = protos_1.google.cloud.tasks.v2.HttpMethod;
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -73,6 +79,49 @@ exports.stripePayment = functions.https.onRequest(async (req, res) => {
             // @ts-ignore
             message = error.toString();
         }
+        functions.logger.error("An error occurred!", { message: message });
+        res.status(401).send({ success: false, error: message });
+    }
+});
+exports.onCallCreated = functions.https.onRequest(async (req, res) => {
+    var _a;
+    const tasksClient = new tasks_1.CloudTasksClient();
+    const callId = req.body.call_id;
+    const expirationTime = req.body.expiry;
+    const projectId = (_a = admin.instanceId().app.options.projectId) !== null && _a !== void 0 ? _a : 'unknown';
+    const huchaToken = process.env.HUCHA_TOKEN;
+    const huchaHost = process.env.HUCHA_HOST;
+    functions.logger.debug(`QueuePath Project: ${projectId}`);
+    const queuePath = tasksClient.queuePath(projectId, 'us-central1', 'call-tasks');
+    const url = `${huchaHost}/expire-call/${huchaToken}`;
+    const task = Task.create({
+        httpRequest: {
+            httpMethod: HttpMethod.POST,
+            body: Buffer.from(JSON.stringify({ 'callId': callId })).toString('base64'),
+            url
+        },
+        scheduleTime: {
+            seconds: expirationTime
+        }
+    });
+    // @ts-ignore
+    task.httpRequest.headers = { 'Content-Type': 'application/json' };
+    try {
+        functions.logger.info(`Creating task for call ${callId} expiring at ${expirationTime}`);
+        // @ts-ignore
+        const createdTaskData = await tasksClient.createTask({ parent: queuePath, task });
+        // @ts-ignore
+        const createdTask = createdTaskData[0];
+        const taskName = createdTask.name;
+        res.json({
+            'task_name': taskName,
+            'call_id': callId
+        });
+        functions.logger.info(`Task ${taskName} created successfully for call ${callId}`);
+    }
+    catch (e) {
+        // @ts-ignore
+        const message = e.toString();
         functions.logger.error("An error occurred!", { message: message });
         res.status(401).send({ success: false, error: message });
     }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,18 @@
 import * as functions from "firebase-functions";
-import {Request, Response} from 'firebase-functions';
+import {Request, Response} from "firebase-functions";
 import Stripe from 'stripe';
+import * as admin from 'firebase-admin';
+
+import {CloudTasksClient} from '@google-cloud/tasks';
+import {google} from '@google-cloud/tasks/build/protos/protos';
+import Task = google.cloud.tasks.v2.Task;
+import HttpRequest = google.cloud.tasks.v2.HttpRequest;
+import HttpMethod = google.cloud.tasks.v2.HttpMethod;
+import Timestamp = google.protobuf.Timestamp;
+import CreateTaskRequest = google.cloud.tasks.v2.CreateTaskRequest;
+
+
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -76,6 +88,52 @@ exports.stripePayment = functions.https.onRequest(async (req: Request, res: Resp
             // @ts-ignore
             message = error.toString();
         }
+        functions.logger.error("An error occurred!", {message: message});
+        res.status(401).send({success: false, error: message});
+    }
+});
+
+exports.onCallCreated = functions.https.onRequest(async (req: Request, res: Response) => {
+    const tasksClient = new CloudTasksClient();
+    const callId = req.body.call_id;
+    const expirationTime = req.body.expiry as number;
+    const projectId = admin.instanceId().app.options.projectId ?? 'unknown';
+    const huchaToken = process.env.HUCHA_TOKEN;
+    const huchaHost = process.env.HUCHA_HOST;
+    functions.logger.debug(`QueuePath Project: ${projectId}`);
+    const queuePath = tasksClient.queuePath(projectId, 'us-central1', 'call-tasks');
+    const url = `${huchaHost}/expire-call/${huchaToken}`;
+
+    const task = Task.create({
+        httpRequest: {
+            httpMethod: HttpMethod.POST,
+            body: Buffer.from(JSON.stringify({'callId': callId})).toString('base64'),
+            url
+        } as HttpRequest,
+        scheduleTime: {
+            seconds: expirationTime
+        } as Timestamp
+    });
+
+    // @ts-ignore
+    task.httpRequest.headers = {'Content-Type': 'application/json'};
+
+    try {
+        functions.logger.info(`Creating task for call ${callId} expiring at ${expirationTime}`);
+
+        // @ts-ignore
+        const createdTaskData = await tasksClient.createTask({parent: queuePath, task} as CreateTaskRequest);
+        // @ts-ignore
+        const createdTask = createdTaskData[0] as Task;
+        const taskName = createdTask.name;
+        res.json({
+            'task_name': taskName,
+            'call_id': callId
+        });
+        functions.logger.info(`Task ${taskName} created successfully for call ${callId}`);
+    } catch (e) {
+        // @ts-ignore
+        const message = e.toString();
         functions.logger.error("An error occurred!", {message: message});
         res.status(401).send({success: false, error: message});
     }
